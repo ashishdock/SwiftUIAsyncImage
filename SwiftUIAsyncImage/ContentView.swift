41f34a58e949fc6d3949a43426c9abedb220d8d0
Ashish Sharma <94269501+ashishdock@users.noreply.github.com>
2022-12-28 12:04:31 +0530

    Completed

--- SwiftUIAsyncImage/ContentView.swift
+++ SwiftUIAsyncImage/ContentView.swift
@@ -7,10 +7,69 @@
 
 import SwiftUI
 
+extension Image {
+    func imageModifier() -> some View {
+        self.resizable().scaledToFit()
+    }
+    
+    func iconModifier() -> some View {
+        self.imageModifier().frame(maxWidth: 128).foregroundColor(.purple).opacity(0.5)
+    }
+}
+
 struct ContentView: View {
+    
+    private let imageURL: String = "https://credo.academy/credo-academy@3x.png"
+    
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        //MARK: - 1. BASIC
+        //AsyncImage(url: URL(string: imageURL))
+        
+        //MARK: - 2. SCALE
+//        AsyncImage(url: URL(string: imageURL), scale: 3.0)
+        
+        //MARK: - 3. PLACEHOLDER
+//        AsyncImage(url: URL(string: imageURL)){ image in
+//            image.imageModifier()
+//        } placeholder: {
+//            Image(systemName: "photo.circle.fill").iconModifier()
+//        }.padding()
+        
+        //MARK: - 4. PHASE
+//        AsyncImage(url: URL(string: imageURL)) { phase in
+//            //SUCCESS: The image was successfully loaded
+//            //FAILURE: The image failed to load with an error
+//            //EMPTY: No image is loaded
+//            if let image = phase.image {
+//                image.imageModifier()
+//            } else if phase.error != nil {
+//                Image(systemName: "ant.circle.fill").iconModifier()
+//            } else {
+//                Image(systemName: "photo.circle.fill").iconModifier()
+//            }
+//        }.padding()
+        
+        //MARK: - 5. ANIMATION
+        AsyncImage(url: URL(string: imageURL), transaction: Transaction(animation: Animation.spring(response: 0.5, dampingFraction: 0.6, blendDuration: 0.25))) { phase in
+            switch phase {
+            case .success(let image):
+                image
+                    .imageModifier()
+//                    .transition(AnyTransition.move(edge: Edge.bottom))
+//                    .transition(AnyTransition.slide)
+                    .transition(AnyTransition.scale)
+            case .failure(_):
+                Image(systemName: "ant.circle.fill").iconModifier()
+            case .empty:
+                Image(systemName: "photo.circle.fill").iconModifier()
+            @unknown default:
+                ProgressView()
+            }
+        }.padding(40)
+        
+        
+        
     }
 }
 
